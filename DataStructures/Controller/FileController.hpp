56dd9f32f26ac648fa76f302ad9f40bdb484ce01
greenRose2018 <brifun13@gmail.com>
2018-01-31 08:53:32 -0700

    created new files

---
+++ DataStructures/Controller/FileController.hpp
@@ -0,0 +1,14 @@
+//
+//  FileController.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include "Data/CrimeData.hpp",<string>,<fstream>,<iostream>,<vector>,<sstream>
+
+#endif /* FileController_hpp */
