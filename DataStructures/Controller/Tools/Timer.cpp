e822ff05bd0dc667c7a09e79febbb3446d7549da
greenRose2018 <brifun13@gmail.com>
2018-01-31 07:59:07 -0700

    created new files and folder, created method headers and initialized method headers

---
+++ DataStructures/Controller/Tools/Timer.cpp
@@ -0,0 +1,40 @@
+//
+//  Timer.cpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Timer.hpp"
+
+using namespace std;
+
+Timer:: Timer()
+{
+}
+
+void Timer:: startTimer()
+{
+    
+}
+
+void Timer:: stopTimer()
+{
+    
+}
+
+void Timer:: resetTimer()
+{
+    
+}
+
+void Timer:: displayInformation();
+{
+    
+}
+
+long Timer:: getTimeInMicroseconds()
+{
+    
+}
