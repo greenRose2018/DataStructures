6ada522213ccee72ffa1b45b30c20d200f69cf4c
greenRose2018 <brifun13@gmail.com>
2018-01-31 08:04:30 -0700

    added code into all methods

--- DataStructures/Controller/Tools/Timer.cpp
+++ DataStructures/Controller/Tools/Timer.cpp
@@ -12,29 +12,32 @@ using namespace std;
 
 Timer:: Timer()
 {
+    executionTime =0;
 }
 
 void Timer:: startTimer()
 {
-    
+    executionTime = clock();
 }
 
 void Timer:: stopTimer()
 {
-    
+    assert(executionTime != 0);
+    executionTime = clock() - executionTime;
 }
 
 void Timer:: resetTimer()
 {
-    
+    executionTime = 0;
 }
 
 void Timer:: displayInformation();
 {
-    
+    cout << "The execution time is: " << executionTime << endl;
+    cout << "In human it is " << double (executionTime)/CLOCKS_PER_SEC << " seconds" <<  endl;
 }
 
 long Timer:: getTimeInMicroseconds()
 {
-    
+    return executionTime;
 }
