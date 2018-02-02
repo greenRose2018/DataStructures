1804b50c6dc638d6d472835e7bba92cc900dd3da
greenRose2018 <brifun13@gmail.com>
2018-02-02 08:50:47 -0700

    created new Array.hpp file

---
+++ DataStructures/Model/Structures/Array.hpp
@@ -0,0 +1,15 @@
+//
+//  Array.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 2/2/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+
+#include <iostream>
+
+
+#endif /* Array_hpp */
