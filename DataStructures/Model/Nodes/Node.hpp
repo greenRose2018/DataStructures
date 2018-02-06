b8f6fa837993f99e6dd250cff91f98f8211d6966
greenRose2018 <brifun13@gmail.com>
2018-02-06 09:46:42 -0700

    set defaultConstructor, setters and getters

--- DataStructures/Model/Nodes/Node.hpp
+++ DataStructures/Model/Nodes/Node.hpp
@@ -23,5 +23,28 @@ public:
     Type getData();
 };
 
+template < class Type>
+Node<Type :: Node()
+{
+    //Needed for default constructor complication
+    //Since when we create a structure it does not have an item to store yet.
+}
 
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->data = data;
+}
+
+template <class Type>
+void NOde<Type> :: setData(Type data)
+{
+    this->data = data;
+}
+
+template <class Type>
+Type Node<Type> :: getData()
+{
+    return data;
+}
 #endif /* Node_hpp */
