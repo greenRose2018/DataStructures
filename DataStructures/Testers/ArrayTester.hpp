3f98785d98cfb36d4667b9925fa303f40a77dd1f
greenRose2018 <brifun13@gmail.com>
2018-02-06 08:27:41 -0700

    created a new folder and file ArrayTester.cpp/.hpp

---
+++ DataStructures/Testers/ArrayTester.hpp
@@ -0,0 +1,14 @@
+//
+//  ArrayTester.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef ArrayTester_hpp
+#define ArrayTester_hpp
+
+#include <iostream>
+
+#endif /* ArrayTester_hpp */
