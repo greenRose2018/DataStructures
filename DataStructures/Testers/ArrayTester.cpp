28e8428b2f1d0b69491f66daa6ae7d90da9fe7ec
greenRose2018 <brifun13@gmail.com>
2018-02-06 09:33:33 -0700

    tested methods, and added a statement when the array is destroyed

--- DataStructures/Testers/ArrayTester.cpp
+++ DataStructures/Testers/ArrayTester.cpp
@@ -21,6 +21,27 @@ void ArrayTester :: testArrayBuild()
     {
         cout << firstArray[j] << endl;
     }
+    
+    Array<int> secondArray(300);
+    cout <<secondArray.getSize()<<endl;
+    cout <<firstArray.getSize()<<endl;
+    
+    Array<int> thirdArray(secondArray);
+    
+    cout << thirdArray.getSize()<<endl;
+    
+    firstArray = thirdArray;
+    cout << firstArray.getSize()<< endl;
+    
+    thirdArray[5] =123;
+    cout << thirdArray[5] << endl;
+    
+    thirdArray[6] = thirdArray.getFromIndex(5);
+    cout <<  thirdArray[6] << endl;
+    
+    thirdArray.setAtIndex(6, 9519);
+    cout << thirdArray[6] << endl;
+    
 }
 
 void ArrayTester:: testAdvancedArray()
