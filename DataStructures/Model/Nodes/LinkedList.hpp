0b119249fcbc64cccd72e43ae16da69dbf17a07a
greenRose2018 <brifun13@gmail.com>
2018-02-14 08:42:13 -0700

    intialized prototypes and made getters and setters

--- DataStructures/Model/Nodes/LinkedList.hpp
+++ DataStructures/Model/Nodes/LinkedList.hpp
@@ -39,5 +39,129 @@ public:
     
 };
 
+template <class Type>
+LinkedList<Type> :: LinkedList()
+{
+    this->front = nullptr;
+    this->end= nullptr;
+    this->size = 0;
+}
+template <class Type>
+LinkedList<Type> :: ~LinkedList()
+{
+    LinearNode<Type> * destroyStructure = front;
+    while (front != nullptr)
+    {
+        front = destroyStructure->getNextNode();
+        delete destroyStructure;
+        destroyStructure = front;
+    }
+}
+template <class Type>
+void LinkedList<Type> :: add(Type item)
+{
+    LinearNode<Type> * newData = new LinearNode<Type>(item);
+    
+    if(this->size==0)
+    {
+        this->front = newData;
+    }
+    else
+    {
+        this->end->setNextNOde(newData);
+    }
+    
+    this->end = newData;
+    this->size += 1;
+}
+template <class Type>
+void LinkedList<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index>= 0 && index <= this->size);
+    if(index == this->size)
+    {
+        add(item);
+    }
+    else
+    {
+        LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
+        if(index == 0)
+        {
+            toBeAdde0>setNextNode(front);
+            front = toBeAdded;
+        }
+        else
+        {
+            LinearNode<Type> * previous = nullptr;
+            LinearNode<Type> * current = front;
+            for(int position = 0; position < index; position++)
+            {
+                previous = current;
+                current = current->getNextNode();
+            }
+            previous->setNextNode(toBeAdded);
+            toBeAdded->setNextNode(current);
+        }
+        this->size++;
+    }
+}
+template<class Type>
+Type LinkeList<Type> :: remove(int index)
+{
+    assert(index >= 0 && index <this->size);
+    
+    LinearNode<Type> * current = front;
+    LinearNode<Type> * toBeRemoved = nullptr;
+    LinearNode<Type> * previous = nullptr;
+    
+    Type removedData;
+    
+    if(index == 0)
+    {
+        toBeRemoved = front;
+        this->front = this->front->getNextNode();
+    }
+    else
+    {
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current->getNextNode();
+        }
+        
+        toBeRemoved = current;
+        if(index == this->size - 1)
+        {
+            previous->setNextNode(nullptr);
+            end = previous;
+        }
+        else
+        {
+            current = toBeRemoved->getNextNode();
+            previous->setNextNode(current);
+        }
+    }
+    this->size -= 1;
+    
+    removedData = tobeRemoved->getData();
+    delete toBeRemoved;
+    return removedData;
+}
+template<class Type>
+LinearNode<Type> * LinkedList<Type> :: getEnd90
+{
+    return this->end;
+}
+template < class Type>
+LinearNode<Type> * LinkedList<Type> :: getFront()
+{
+    return this->front;
+}
+template<class Type>
+int LinearNode<Type> :: getSize() const
+{
+    return this->size;
+}
+
 
 #endif /* LinkedList_hpp */
