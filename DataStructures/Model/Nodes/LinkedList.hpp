a9090792df947b1b8c63ba51c4720fff40619520
greenRose2018 <brifun13@gmail.com>
2018-02-14 08:41:14 -0700

    created new file LinkedList, added imports and template class

---
+++ DataStructures/Model/Nodes/LinkedList.hpp
@@ -0,0 +1,24 @@
+//
+//  LinkedList.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 2/14/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+#inclued "../Structures/List.hpp"
+using namespace std;
+
+template <class Type>
+class LinkedList : public List<Type>
+{
+protected:
+public:
+    
+};
+
+
+#endif /* LinkedList_hpp */
