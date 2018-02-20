99bb2ab2577807c05e0c836a99caa18ea87ad88b
greenRose2018 <brifun13@gmail.com>
2018-02-20 08:09:38 -0700

    added method getFromIndex

--- DataStructures/Model/Nodes/LinkedList.hpp
+++ DataStructures/Model/Nodes/LinkedList.hpp
@@ -106,6 +106,23 @@ void LinkedList<Type> :: addAtIndex(int index, Type item)
     }
 }
 template<class Type>
+Type LinkedList<Type>:: getFromIndex(int index)
+{
+    assert(index >= 0 && index < this->size);
+    Type data;
+    
+    LinearNode<Type> * current = front;
+    
+    for( int position = 0 ; position < index; position++)
+    {
+        current = current->getNextNode();
+    }
+    
+    data = current->getData();
+    return data;
+}
+
+template<class Type>
 Type LinkedList<Type> :: remove(int index)
 {
     assert(index >= 0 && index <this->size);
