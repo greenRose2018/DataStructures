83eee77648a829c47ce6382e08eb1b96e0438767
greenRose2018 <brifun13@gmail.com>
2018-02-26 09:21:22 -0700

    created file CircularList, and added prototypes

---
+++ DataStructures/Model/Structures/CircularList.hpp
@@ -0,0 +1,62 @@
+//
+//  CircularList.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef CircularList_hpp
+#define CircularList_hpp
+
+#include "List.hpp"
+#include "../../Nodes/DoubleNode.hpp"
+
+template <class Type>
+class CircularList : public List<Type>
+{
+private:
+    DoubleNode<Type> * front;
+    DoubleNode<Type> * end;
+    
+    DoubleNode<Type> * findNode(int index);
+    
+public:
+    CircularList();
+    ~CircularList();
+    
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type remove(int index);
+    Type getFromIndex(int index);
+    int getSize() const;
+};
+
+    
+template <class Type>
+void CircularList<Type> :: add(Type item)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#endif /* CircularList_hpp */
