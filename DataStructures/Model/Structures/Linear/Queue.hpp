d12de9f3186521502df3b315d69356069b9b37f6
greenRose2018 <brifun13@gmail.com>
2018-02-27 08:12:13 -0700

    created new files Queue, and Stack

---
+++ DataStructures/Model/Structures/Linear/Queue.hpp
@@ -0,0 +1,14 @@
+//
+//  Queue.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 2/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Queue_hpp
+#define Queue_hpp
+
+#include <stdio.h>
+
+#endif /* Queue_hpp */
---
+++ DataStructures/Model/Structures/Linear/Stack.hpp
@@ -0,0 +1,14 @@
+//
+//  Stack.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 2/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Stack_hpp
+#define Stack_hpp
+
+#include <stdio.h>
+
+#endif /* Stack_hpp */
