b121a50bebc0847c92078ba80488b7877cf7e67e
greenRose2018 <brifun13@gmail.com>
2018-02-28 10:02:39 -0700

    created files TestingStacksAndQueues .hpp/.cpp, added prototypes

---
+++ DataStructures/Testers/TestingStacksAndQueues.cpp
@@ -0,0 +1,10 @@
+//
+//  TestingStacksAndQueues.cpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 2/28/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "TestingStacksAndQueues.hpp"
+
