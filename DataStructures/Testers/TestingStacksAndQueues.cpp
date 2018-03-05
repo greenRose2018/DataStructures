90f8e29553c6b902c939852e3cdded932150ffe5
greenRose2018 <brifun13@gmail.com>
2018-03-05 07:55:35 -0700

    added method stackVersusList() with code

--- DataStructures/Testers/TestingStacksAndQueues.cpp
+++ DataStructures/Testers/TestingStacksAndQueues.cpp
@@ -8,3 +8,32 @@
 
 #include "TestingStacksAndQueues.hpp"
 
+void TestingStacksAndQueues :: stackVersusList()
+{
+    Stack<int> numberStack;
+    LinkedList<int> numberList;
+    Timer stackTimer, listTimer;
+    
+    for(int index = 0; index< 10; index++)
+    {
+        numberStack.add(index);
+        numberStack.add(index);
+    }
+    
+    cout << "Get the third from the last item from the structure" << endl;
+    int index = 0;
+    int value = -99;
+    stackTimer.startTimer()
+    
+    while(index<9)
+    {
+        cout << numberStack.pop() << endl;
+        index++;
+    }
+    
+    stackTimer.stopTimer()
+    index = 0;
+    listTimer.startTimer();
+    value = numberList.getFromIndex(7);
+    listTimer.stopTimer();
+}
