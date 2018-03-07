7accdc112b168079739c9d75e86ec5f8115ab146
greenRose2018 <brifun13@gmail.com>
2018-03-07 09:46:20 -0700

    created CircularListTester, and set it up

---
+++ DataStructures/Testers/CircularListTester.cpp
@@ -0,0 +1,24 @@
+//
+//  CircularListTester.cpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 3/7/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "CircularListTester.hpp"
+
+CircularListTester :: CircularListTester()
+{
+    
+}
+
+void CircularListTester :: test()
+{
+    
+}
+
+void CircularListTester:: secondTest()
+{
+    
+}
---
+++ DataStructures/Testers/CircularListTester.hpp
@@ -0,0 +1,24 @@
+//
+//  CircularListTester.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 3/7/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef CircularListTester_hpp
+#define CircularListTester_hpp
+
+#include <iostream>
+#include <string>
+#include "../Controller/Tools/Timer.hpp"
+
+class CircularListTester
+{
+public:
+    CircularListTester();
+    void test();
+    void secondTest();
+};
+
+#endif /* CircularListTester_hpp */
