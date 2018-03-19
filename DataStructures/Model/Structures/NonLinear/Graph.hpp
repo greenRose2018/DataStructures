5a4db104f9a7d8828eb0737948a1c8e688d284a8
greenRose2018 <brifun13@gmail.com>
2018-03-19 10:00:36 -0600

    added methods
    hasUndirectedConnection()
    areConnected()

--- DataStructures/Model/Structures/NonLinear/Graph.hpp
+++ DataStructures/Model/Structures/NonLinear/Graph.hpp
@@ -154,4 +154,26 @@ void Graph<Type> :: addEdgeCost(int source, int target, int cost)
     adjacencyMatrix[target][source] = cost;
 }
 
+template<class Type>
+bool Graph<Type> :: hasUndirectedConnection(int source,int target) const
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source][target] || adjacencyMatrix[target][source];
+    
+    return isAnEdge;
+}
+
+template<class Type>
+bool Graph<Type> :: areConnected(int source, int target) const
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source][target];
+    
+    return isAnEdge;
+}
+
 #endif /* Graph_hpp */
