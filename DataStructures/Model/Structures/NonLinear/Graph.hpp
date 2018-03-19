16928fcf6c40e343eabf40b092b455d03ab5372b
greenRose2018 <brifun13@gmail.com>
2018-03-19 09:09:57 -0600

    declared methods and a variable
    var: MAXIMUM
    Graph()
    size() const
    operator[]()
    operator[]() const

--- DataStructures/Model/Structures/NonLinear/Graph.hpp
+++ DataStructures/Model/Structures/NonLinear/Graph.hpp
@@ -53,4 +53,34 @@ public:
     void breadthFirstTraversal(Graph<Type> & graph,int vertex);
     int costTraversal(Graph<Type> & graph,int vertex);
 };
+
+template <class Type>
+const int Graph<Type> :: MAXIMUM;
+
+template <class Type>
+Graph<Type> :: Graph()
+{
+    this->vertexCount = 0;
+}
+
+template <class Type>
+int Graph<Type> :: size() const
+{
+    return vertexCount;
+}
+
+template <class Type>
+Type& Graph<Type> :: operator[](int vertex)
+{
+    assert(vertex  < vertexCount);
+    return graphData[vertex];
+}
+
+template <class Type>
+TypeGraph<Type> :: operator[](int vertex)const
+{
+    assert(vertex < vertexCount);
+    return graphData[vertex];
+}
+
 #endif /* Graph_hpp */
