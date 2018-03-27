b9dc279c9d8592316f5b953b228ab817f4715809
greenRose2018 <brifun13@gmail.com>
2018-03-27 09:15:24 -0600

    created BinaryTreeTester class and setup

---
+++ DataStructures/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,14 @@
+//
+//  BinaryTreeTester.cpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "BinaryTreeTester.hpp"
+
+void BinaryTreeTester:: testNodes()
+{
+    std::cout << "is it working?" << std::endl;
+}
---
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,21 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include "../Model/Nodes/BinaryTreeNode.hpp"
+#include <iostream>
+
+class BinaryTreeTester
+{
+public:
+    void testNodes();
+};
+
+#endif /* BinaryTreeTester_hpp */
