e0a3071006d4ddc1fd9b8fccb46cb2a9ba01c043
greenRose2018 <brifun13@gmail.com>
2018-04-17 08:36:33 -0600

    Created Method headers for AVLTree

--- DataStructures/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -15,6 +15,22 @@ template <class Type>
 class AVLTree : public BinarySearchTree<Type>
 {
 private:
+    BinaryTreeNode<Type> * leftRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightdifference(BinaryTreeNode<Type>* parent);
+    
 public:
+    ABLTree();
+    
+    void  insert(Type itemToInsert);
+    void remove(Type value);
 };
 #endif /* AVLTree_hpp */
