6ebca8d8e38dd8651292ba0684fc3fb17e478f6b
greenRose2018 <brifun13@gmail.com>
2018-04-17 08:36:07 -0600

    created new header file AVLTree
    inherits from BinarySearchTree<Type>

---
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -0,0 +1,20 @@
+//
+//  AVLTree.hpp
+//  DataStructures
+//
+//  Created by Morales, Brittney on 4/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef AVLTree_hpp
+#define AVLTree_hpp
+
+#include "BinarySearchTree.hpp"
+
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+public:
+};
+#endif /* AVLTree_hpp */
