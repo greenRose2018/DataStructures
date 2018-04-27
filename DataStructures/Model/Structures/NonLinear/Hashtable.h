8fc7a01e619fd8a97f498d4496f78ea877999973
greenRose2018 <brifun13@gmail.com>
2018-04-27 09:32:20 -0600

    added more headers and created stubs for header

--- DataStructures/Model/Structures/NonLinear/Hashtable.h
+++ DataStructures/Model/Structures/NonLinear/Hashtable.h
@@ -15,18 +15,24 @@ template <class Type>
 class Hashtable
 {
 private:
-    HashNode<Type> * internalStorage;
-    int size;
+    HashNode<Type> ** internalStorage;
+    long size;
+    double loadFactor;
+    
+    long getNextPrime(long current);
+    void resize();
 public:
     Hashtable();
     ~Hashtable();
+    void insert(Type data);
 };
 
 template<class Type>
 HashTable<Type> :: Hashtable()
 {
-    internalStorage = new HashNode<Type>[101];
+    internalStorage = new HashNode<Type>*[101];
     size = 0;
+    loadFactor = .80;
 }
 
 template<class Type>
@@ -34,4 +40,17 @@ Hashtable<Type> :: ~Hashtable()
 {
     delete [] internalStorage;
 }
+
+template<class Type>
+long Hashtable<Type> :: getNextPrime(long currentNumber)
+{
+    return -1;
+}
+
+template<class Type>
+void Hashtable<Type> :: resize()
+{
+    
+}
+
 #endif /* Hashtable_h */
